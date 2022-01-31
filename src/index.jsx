ebc2903362a91defabed4268913437faf1fd5119
Viacheslav Kuznetsov <via.kuznetsov@gmail.com>
2022-01-31 14:49:52 +0300

    changing node version to 12x

--- src/index.jsx
+++ src/index.jsx
@@ -3,16 +3,10 @@ import React from 'react';
 import i18next from 'i18next';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 
 import App from './App.jsx';
 import store from './slices/index.js';
 import resources from './resources/index.js';
-
-const rollbarConfig = {
-  accessToken: 'POST_CLIENT_ITEM_ACCESS_TOKEN',
-  environment: 'production',
-};
 
 export default async () => {
   await i18next.init({
@@ -22,13 +16,9 @@ export default async () => {
   });
 
   ReactDOM.render(
-    <RollbarProvider config={rollbarConfig}>
-      <ErrorBoundary>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </ErrorBoundary>
-    </RollbarProvider>,
+    <Provider store={store}>
+      <App />
+    </Provider>,
     document.getElementById('chat'),
   );
 };
