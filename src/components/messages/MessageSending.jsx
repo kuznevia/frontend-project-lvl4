a2182a497b1f0fd5672cf91076818d94109bce44
Viacheslav Kuznetsov <via.kuznetsov@gmail.com>
2022-02-23 00:23:01 +0300

    fixing mentors comments step 6.1

--- src/components/messages/MessageSending.jsx
+++ src/components/messages/MessageSending.jsx
@@ -6,6 +6,7 @@ import { ApiContext } from '../../contexts/ApiContextProvider.jsx';
 
 const MessageSending = ({ activeChannelId }) => {
   const [inputText, setInputText] = useState('');
+  const [inputDisabled, setInputDisabled] = useState(false);
   const { sendMessage } = useContext(ApiContext);
   const activeUser = useSelector((state) => state.messages.activeUser);
 
@@ -20,29 +21,29 @@ const MessageSending = ({ activeChannelId }) => {
     if (inputText === '') {
       return;
     }
+    setInputDisabled(true);
     await sendMessage({
       text: filter.clean(inputText),
       user: activeUser,
       channelId: activeChannelId,
     });
+    setInputDisabled(false);
     setInputText('');
   };
 
   return (
     <form className="py-1 border rounded-2" onSubmit={handleFormSubmit}>
-      <fieldset disabled>
-        <div className="input-group has-validation">
-          <input id="message-input-box" className="border-0 p-0 px-2 form-control" value={inputText} aria-label="Новое сообщение" onChange={handleInputChange} placeholder="Введите сообщение..." />
-          <div className="input-group-append">
-            <button type="submit" className="btn btn-group-vertical">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor">
-                <path fillRule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
-              </svg>
-              <span className="d-none">{t('actions.send')}</span>
-            </button>
-          </div>
+      <div className="input-group has-validation">
+        <input id="message-input-box" className="border-0 p-0 px-2 form-control" value={inputText} aria-label="Новое сообщение" onChange={handleInputChange} placeholder="Введите сообщение..." disabled={inputDisabled} />
+        <div className="input-group-append">
+          <button type="submit" className="btn btn-group-vertical">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor">
+              <path fillRule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
+            </svg>
+            <span className="d-none">{t('actions.send')}</span>
+          </button>
         </div>
-      </fieldset>
+      </div>
     </form>
   );
 };
